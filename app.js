1dd34a4a08c9ba962b903669d65e30d40760aadb
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-25 01:14:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+const fetchCountryByName = (name)=>{
+    const url =`https://restcountries.com/v3.1/name/${name}`;
+    fetch(url).then((res) =>{
+        if(!res.ok){
+            renderError(`Someting went wrong ${res.status}`);
+            throw new Error();
+        }
+        return res.json()
+    })
+    .then((data)=> {
+        renderCountries(data)
+    })
+   .catch((err) =>console.log(err));
+};
+
+const renderError =()=>{
+    const countryDiv = document.querySelector(".countries");
+    countryDiv.innerHTML += ` <h2> Countries can not fetched </h2> `
+    
+}
+const renderCountries = (data)=>{
+    console.log(data)
+    const countryDiv = document.querySelector(".countries");
+    const {capital, currencies,flags:{svg},languages, name:{common}, region} =data[0]
+    countryDiv.innerHTML += `<div class="card" style="width: 18rem;">
+    <img src="${svg}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${common}</h5>
+      <p class="card-text">${region}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+    </ul>
+  </div>
+  `
+
+}
+
+fetchCountryByName("suisse")
+fetchCountryByName("usa")
+fetchCountryByName("germany")
